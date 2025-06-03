5b70462667b20c949dcd5173b2c90494f1c2f7e2
David Falk <spreetin@protonmail.com>
2025-06-03 09:26:08 +0200

    Bugfixes on install script

--- install_config.sh
+++ install_config.sh
@@ -34,19 +34,19 @@ GENTOO_APPS="app-shells/zsh app-editors/neovim app-misc/mc media-gfx/imagemagick
 FEDORA_APPS="${APPLICATIONS} ImageMagick"
 APPLICATIONS="${APPLICATIONS} imagemagick"
 
-if [ -n "${XAPPS}"]; then
+if [ -n "${XAPPS}" ]; then
     APPLICATIONS="${APPLICATIONS} kitty"
     GENTOO_APPS="${GENTOO_APPS} x11-terms/kitty"
 fi
 
 # Install necesary packages, if not already installed
-if [ -x "$(command -v emerge)"]; then
+if [ -x "$(command -v emerge)" ]; then
     sudo emerge -avn ${GENTOO_APPS}
-elif [ -x "$(command -v pacman)"]; then
+elif [ -x "$(command -v pacman)" ]; then
     sudo pacman -S ${APPLICATIONS}
-elif [ -x "$(command -v apt)"]; then
+elif [ -x "$(command -v apt)" ]; then
     sudo apt install ${APPLICATIONS}
-elif [ -x "$(command -v dnf)"]; then
+elif [ -x "$(command -v dnf)" ]; then
     sudo dnf install ${FEDORA_APPS}
 else
     echo 'Error: unknown package management system' >&2
@@ -57,34 +57,34 @@ fi
 chsh -s $(which zsh)
 
 # Install oh-my-zsh
-if [ -x "$(command -v curl)"]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-elif [ -x "$(command -v wget)"]; then
-    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -x "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+elif [ -x "$(command -v wget)" ]; then
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # Symlink all configs, moving old configs away (to filename.bak_dotfile)
-if [ -n "${XAPPS}"]; then
-    if [ -e "~/.config/kitty"]; then
+if [ -n "${XAPPS}" ]; then
+    if [ -e ~/.config/kitty ]; then
         mv ~/.config/kitty ~/.config/kitty.bak_dotfiles
     fi
     ln -s "${SCRIPT_PATH}/kitty" ~/.config/kitty
 fi
-if [ -e "~/.zshrc"]; then
+if [ -e ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.bak_dotfiles
 fi
 ln -s "${SCRIPT_PATH}/zsh/.zshrc" ~/.zshrc
 mkdir -p ~/.oh-my-zsh/custom/
-if [ -e "~/.oh-my-zsh/custom/alias.zsh"]; then
+if [ -e ~/.oh-my-zsh/custom/alias.zsh ]; then
     mv ~/.oh-my-zsh/custom/alias.zsh ~/.oh-my-zsh/custom/alias.zsh.bak_dotfiles
 fi
 ln -s "${SCRIPT_PATH}/oh-my-zsh/custom/alias.zsh" ~/.oh-my-zsh/custom/alias.zsh
 mkdir -p ~/.config/nvim/
-if [ -e "~/.config/nvim/lua"]; then
+if [ -e ~/.config/nvim/lua ]; then
     mv ~/.config/nvim/lua ~/.config/nvim/lua.bak_dotfiles
 fi
 ln -s "${SCRIPT_PATH}/neovim/lua" ~/.config/nvim/lua
-if [ -e "~/.config/nvim/init.lua"]; then
+if [ -e ~/.config/nvim/init.lua ]; then
     mv ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak_dotfiles
 fi
 ln -s "${SCRIPT_PATH}/neovim/init.lua" ~/.config/nvim/init.lua
